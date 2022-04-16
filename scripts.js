c89d119f9bf546c81255ebd29ad19fd8e49c5cff
viveroa2291 <viveroa2291@uwec.edu>
2022-04-15 20:53:43 -0500

    Not Finished
    Committing

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
-    const previousButton = document.getElementById('previous-button'); // The back button
-    const nextButton = document.getElementById('next-button'); // The next button
-    const headerDiv = document.getElementById('header');
-    const descriptionDiv = document.getElementById('descriptionBody');
+const previousButton = document.getElementById('previous-button'); // The back button
+const nextButton = document.getElementById('next-button'); // The next button
+const headerDiv = document.getElementById('header'); // header title
+const images = document.getElementById('images');
+const descriptionDiv = document.getElementById('descriptionBody');
 
-    const headerTitles = ["Home", "About", "Contact", "Story", "Projects"];
-    const headerDescritpions = ["This is the home page", "This is the about page", "This is the contact page", "This is the story page", "This is the projects page."];
-    const linkTitles = ["home.html", "about.html", "contact.html", "story.html", "projects.html"];
-    let s = 0;
+const headerTitles = ["Door County", "Acapulco", "United States Capitol", "Cedar Point", "Chicago", "South Dakota"];
+const headerDescritpions = ["This is an image of Anderson's Dock in Door County Wisconsin", "This is an image of Acapulco, Mexico driving into.", "This is an image of the United States Capitol in Washington, DC.", "This is an image of Chicago taken from the highway.", "This is an image of buffalos taken in South Dakota."];
+const imageSource = ["images/doorCounty.jpeg", "images/acapulco.jpeg", "images/capital.jpeg", "images/cedarPoint.jpeg", "images/chicago.jpeg", "images/southDakota.jpeg"];
+const linkTitles = ["home.html", "about.html", "contact.html", "story.html", "projects.html"];
+let s = 0;
 
-    function advance(delta) {
-        s = (s + delta + headerTitles.length) % headerTitles.length;
-        descriptionDiv.innerHTML = headerDescritpions[s];
-        headerDiv.innerHTML = headerTitles[s];
-        headerDiv.setAttribute("href", linkTitles[s]);
-    }
-    previousButton.addEventListener('click', () => advance(-1));
-    nextButton.addEventListener('click', () => advance(1));
+function advance(delta) {
+    s = (s + delta + headerTitles.length) % headerTitles.length;
+   // descriptionDiv.innerHTML = headerDescritpions[s];
+    headerDiv.innerHTML = headerTitles[s];
+   // headerDiv.setAttribute("href", linkTitles[s]);
+    images.src = imageSource[s];
+}
+previousButton.addEventListener('click', () => advance(-1));
+nextButton.addEventListener('click', () => advance(1));
 
-    advance(0);
+advance(0);
